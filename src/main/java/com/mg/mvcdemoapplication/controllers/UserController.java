2254dec58893e6292842db97ab489a0d0154ef6b
mgd <mgd@posteo.net>
2024-09-26 21:08:06 +0100

    Added functionality to save users to the database

--- src/main/java/com/mg/mvcdemoapplication/controllers/UserController.java
+++ src/main/java/com/mg/mvcdemoapplication/controllers/UserController.java
@@ -1,15 +1,20 @@
 package com.mg.mvcdemoapplication.controllers;
 
+import com.mg.mvcdemoapplication.entities.UserEntity;
 import com.mg.mvcdemoapplication.repositories.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class UserController {
 
-    private UserEntityRepository userEntityRepository;
+    private final UserEntityRepository userEntityRepository;
 
     @Autowired
     public UserController(UserEntityRepository userEntityRepository) {
@@ -25,5 +30,19 @@ public class UserController {
     public String getAllUsers(Model model) {
         model.addAttribute("users", userEntityRepository.findAll());
         return "users";
+    }
+
+    @GetMapping("/add-user")
+    public String addUser(Model model) {
+        UserEntity userEntity = new UserEntity();
+        model.addAttribute("userEntity", userEntity);
+        return "add-user";
+    }
+
+    @PostMapping("/save-user")
+    public String saveUser(@ModelAttribute("userEntity") UserEntity userEntity) {
+        userEntity.setCreatedAt(LocalDateTime.now());
+        userEntityRepository.save(userEntity);
+        return "redirect:/users";
     }
 }
